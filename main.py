7b3728f9c97730c426cbc2287f3b81d956eb83e5
Italo Nicola <italo.niccola@gmail.com>
2017-07-13 07:52:59 +0000

    MVP

---
+++ main.py
@@ -0,0 +1,53 @@
+import math
+from PIL import Image
+
+def brightness(color):
+    return (color['r'] / 255) * 0.3 + (color['g'] / 255) * 0.59 + (color['b'] / 255) * 0.11
+
+#characters = ['g', 's', 'a', 'e', 'o', 'c', 'b', 'd', 'p', 'q', 'h', 'u', 'n', 'm', 'r', 't', 'f', 'i', 'j', 'l', 'k', 'x', 'z', 'w', 'v', 'y', '=', '-']
+#characters = ['@', '&', '#', '8', '0', 'g', 's', 'a', 'e', 'o', 'c', 'b', 'd', 'p', 'q', 'h', 'u', 'n', 'm', '*', 'r', 't', 'f', 'i', 'j', 'l', 'k', 'x', 'z', 'w', 'v', 'y', '=', '-']
+#characters = ['@', '0', 'm', 'w', 'a', 'e', 'n', 'u', 's', 'x', 'l', 'i', 'v', '*', 'r', '-']
+characters = ['@', '&', '0', 'm', 'w', 'a', 'e', 'n', 'u', 'x', 'l', 'i', 'v', '*', 'r', '~', '-']
+characters.reverse()
+
+BLOCK_SIZE = 2
+BLOCK_WIDTH = 4
+BLOCK_HEIGHT = 8
+RANGE_FACTOR = (255 * 3) / len(characters)
+
+im = Image.open("joelho.jpg")
+
+block_map = []
+
+block_x = 0
+while block_x + BLOCK_HEIGHT <= im.height:
+    block_map.append([])
+    block_y = 0
+    while block_y + BLOCK_WIDTH <= im.width:
+        color_total = {'r': 0, 'g': 0, 'b': 0}
+        for i in range(BLOCK_WIDTH):
+            for j in range(BLOCK_HEIGHT):
+                color_total['r'] += im.getpixel((block_y + i, block_x + j))[0]
+                color_total['g'] += im.getpixel((block_y + i, block_x + j))[1]
+                color_total['b'] += im.getpixel((block_y + i, block_x + j))[2]
+
+        pixels_count = BLOCK_WIDTH * BLOCK_HEIGHT
+        color_avg = {'r': color_total['r'] / pixels_count, 'g': color_total['g'] / pixels_count, 'b': color_total['b'] / pixels_count}
+
+        block_map[-1].append(color_avg)
+
+        # NOT BRIGHTNESS
+        #color_avg['r'] /= RANGE_FACTOR
+        #color_avg['g'] /= RANGE_FACTOR
+        #color_avg['b'] /= RANGE_FACTOR
+        #print((characters[int(color_avg['r']) + int(color_avg['g']) + int(color_avg['b'])]), end = "")
+
+        # BRIGHTNESS
+        print((characters[int((brightness(color_avg) * len(characters)))]), end = "")
+
+        block_y += BLOCK_WIDTH
+
+    print()
+    block_x += BLOCK_HEIGHT
+
+im.save("out.bmp")
